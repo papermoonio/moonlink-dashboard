7fb3341771e015f3ac18a1ecfab96d0abf241458
Albertov19 <albertoviera@gmail.com>
2022-07-29 15:14:04 +0200

    update data feeds

--- components/datafeed-page.js
+++ components/datafeed-page.js
@@ -1,220 +1,90 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'semantic-ui-react';
 import ProxyInstance from '../ethereum/feed';
 const addresses = require('../ethereum/addresses');
 
-class Table extends Component {
-   // Nextjs uses this function to render this first server-side
-   static async getInitialProps() {
-      onUpdate();
-   }
+const table = () => {
+  const [feedsData, setFeedsData] = useState(Array());
+  const { Header, Row, HeaderCell, Body, Cell } = Table;
 
-   // Set Initial State
-   state = {
-      errorMessage: '',
-      btcusd: 'N/A',
-      ethusd: 'N/A',
-      dotusd: 'N/A',
-      ksmusd: 'N/A',
-      aaveusd: 'N/A',
-      algousd: 'N/A',
-      bandusd: 'N/A',
-      linkusd: 'N/A',
-      sushiusd: 'N/A',
-      uniusd: 'N/A',
-      btcUpdate: 'N/A',
-      ethUpdate: 'N/A',
-      dotUpdate: 'N/A',
-      ksmUpdate: 'N/A',
-      aaveUpdate: 'N/A',
-      algoUpdate: 'N/A',
-      bandUpdate: 'N/A',
-      linkUpdate: 'N/A',
-      sushiUpdate: 'N/A',
-      uniUpdate: 'N/A',
-   };
+  useEffect(async () => {
+    const getPriceData = async () => {
+      const feeds = Object.keys(addresses);
+      let data = Array();
 
-   async componentDidMount() {
-      this.onUpdate();
-   }
+      for (const feed of feeds) {
+        const address = addresses[feed];
 
-   componentWillUnmount() {
-      clearInterval(this.intervalID);
-   }
+        // Get price data from proxy contract
+        if (address) {
+          try {
+            const contractInstance = ProxyInstance(address);
+            const dec = await contractInstance.decimals();
+            const info = await contractInstance.latestRoundData();
+            const price = info[1] / Math.pow(10, dec);
+            const epoch = new Date(info[3].toNumber() * 1000);
+            const date = `${epoch.getFullYear()}/${('00' + (epoch.getMonth() + 1)).slice(-2)}/${(
+              '00' + epoch.getDate()
+            ).slice(-2)} -- 
+            ${('00' + epoch.getHours()).slice(-2)}:${('00' + epoch.getMinutes()).slice(-2)}`;
 
-   onUpdate = async () => {
-      // BTC
-      const [btcPrice, btcUpdate] = await this.getPriceData(addresses.btcusd);
-      // ETH
-      const [ethPrice, ethUpdate] = await this.getPriceData(addresses.ethusd);
-      // DOT
-      const [dotPrice, dotUpdate] = await this.getPriceData(addresses.dotusd);
-      // KSM
-      const [ksmPrice, ksmUpdate] = await this.getPriceData(addresses.ksmusd);
-      // AAVE
-      const [aavePrice, aaveUpdate] = await this.getPriceData(
-         addresses.aaveusd
-      );
-      // ALGO
-      const [algoPrice, algoUpdate] = await this.getPriceData(
-         addresses.algousd
-      );
-      // BAND
-      const [bandPrice, bandUpdate] = await this.getPriceData(
-         addresses.bandusd
-      );
-      // LINK
-      const [linkPrice, linkUpdate] = await this.getPriceData(
-         addresses.linkusd
-      );
-      // SUSHI
-      const [sushiPrice, sushiUpdate] = await this.getPriceData(
-         addresses.sushiusd
-      );
-      // UNI
-      const [uniPrice, uniUpdate] = await this.getPriceData(addresses.uniusd);
-
-      this.setState({
-         // Set Price
-         btcusd: btcPrice.toFixed(2),
-         ethusd: ethPrice.toFixed(2),
-         dotusd: dotPrice.toFixed(2),
-         ksmusd: ksmPrice.toFixed(2),
-         aaveusd: aavePrice.toFixed(2),
-         algousd: algoPrice.toFixed(2),
-         bandusd: bandPrice.toFixed(2),
-         linkusd: linkPrice.toFixed(2),
-         sushiusd: sushiPrice.toFixed(2),
-         uniusd: uniPrice.toFixed(2),
-
-         // Set Last Updated
-         btcUpdate: btcUpdate,
-         ethUpdate: ethUpdate,
-         dotUpdate: dotUpdate,
-         ksmUpdate: ksmUpdate,
-         aaveUpdate: aaveUpdate,
-         algoUpdate: algoUpdate,
-         bandUpdate: bandUpdate,
-         linkUpdate: linkUpdate,
-         sushiUpdate: sushiUpdate,
-         uniUpdate: uniUpdate,
-      });
-
-      this.intervalID = setTimeout(this.onUpdate.bind(this), 5000);
-   };
-
-   getPriceData = async (address) => {
-      // Get price data from proxy contract
-      try {
-         const contractInstance = ProxyInstance(address);
-         const dec = await contractInstance.decimals();
-         const info = await contractInstance.latestRoundData();
-         const price = info[1] / Math.pow(10, dec);
-         const epoch = new Date(info[3].toNumber() * 1000);
-         const date = `${epoch.getFullYear()}/
-         ${('00' + (epoch.getMonth() + 1)).slice(-2)}/
-         ${('00' + epoch.getDate()).slice(-2)} -- 
-         ${('00' + epoch.getHours()).slice(-2)}:
-         ${('00' + epoch.getMinutes()).slice(-2)}:
-         ${('00' + epoch.getSeconds()).slice(-2)}`;
-
-         return [price, date];
-      } catch (error) {
-         // Could not fetch price return error
-         console.log(error);
-         return [0, 'N/A'];
+            data[feed] = { price: price.toFixed(3), date: date };
+          } catch (error) {
+            // Could not fetch price return error
+            console.log(error);
+            data[feed] = { price: 0, date: 'N/A' };
+          }
+        } else {
+          console.log('Price feed not available');
+          data[feed] = { price: 0, date: 'N/A' };
+        }
       }
-   };
 
-   render() {
-      return (
-         <div>
-            <h3>Data Price Feed</h3>
-            <p>
-               Information displayed in the following table corresponds to
-               on-chain price data stored in the Moonbase Alpha TestNet!
-            </p>
-            <table className='ui celled table'>
-               <thead>
-                  <tr>
-                     <th>Token Pair</th>
-                     <th>Price</th>
-                     <th>Last Updated</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>BTC / USD</td>
-                     <td data-label='Price'>$ {this.state.btcusd}</td>
-                     <td data-label='Last Updated'>{this.state.btcUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>ETH / USD</td>
-                     <td data-label='Price'>$ {this.state.ethusd}</td>
-                     <td data-label='Last Updated'>{this.state.ethUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>DOT / USD</td>
-                     <td data-label='Price'>$ {this.state.dotusd}</td>
-                     <td data-label='Last Updated'>{this.state.dotUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>KSM / USD</td>
-                     <td data-label='Price'>$ {this.state.ksmusd}</td>
-                     <td data-label='Last Updated'>{this.state.ksmUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>AAVE / USD</td>
-                     <td data-label='Price'>$ {this.state.aaveusd}</td>
-                     <td data-label='Last Updated'>{this.state.aaveUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>ALGO / USD</td>
-                     <td data-label='Price'>$ {this.state.algousd}</td>
-                     <td data-label='Last Updated'>{this.state.algoUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>BAND / USD</td>
-                     <td data-label='Price'>$ {this.state.bandusd}</td>
-                     <td data-label='Last Updated'>{this.state.bandUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>LINK / USD</td>
-                     <td data-label='Price'>$ {this.state.linkusd}</td>
-                     <td data-label='Last Updated'>{this.state.linkUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>SUSHI / USD</td>
-                     <td data-label='Price'>$ {this.state.sushiusd}</td>
-                     <td data-label='Last Updated'>{this.state.sushiUpdate}</td>
-                  </tr>
-               </tbody>
-               <tbody>
-                  <tr>
-                     <td data-label='Token Pair'>UNI / USD</td>
-                     <td data-label='Price'>$ {this.state.uniusd}</td>
-                     <td data-label='Last Updated'>{this.state.uniUpdate}</td>
-                  </tr>
-               </tbody>
-            </table>
-         </div>
-      );
-   }
-}
+      setFeedsData(data);
+      setInterval(async () => await getPriceData(), 5000);
+    };
 
-export default Table;
+    await getPriceData();
+  }, []);
+
+  const renderRows = () => {
+    const feeds = Object.keys(addresses);
+    if (feeds.length !== 0) {
+      return feeds.map((feed) => {
+        if (feedsData[feed]) {
+          return (
+            <Row key={feed}>
+              <Cell>{feed.slice(0, feed.lastIndexOf('usd')).toUpperCase() + ' / USD'}</Cell>
+              <Cell>{feedsData[feed].price}</Cell>
+              <Cell>{feedsData[feed].date}</Cell>
+              <Cell>{addresses[feed]}</Cell>
+            </Row>
+          );
+        }
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h3>Data Price Feed</h3>
+      <p>
+        Information displayed in the following table corresponds to on-chain price data stored in
+        the Moonbase Alpha TestNet!
+      </p>
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell> Token Pair </HeaderCell>
+            <HeaderCell> Price </HeaderCell>
+            <HeaderCell> Last Updated </HeaderCell>
+            <HeaderCell> Proxy Address </HeaderCell>
+          </Row>
+        </Header>
+        <Body>{renderRows()}</Body>
+      </Table>
+    </div>
+  );
+};
+
+export default table;
--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,117 @@
-import React, { Component } from 'react';
-import { Container, Button, Menu } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Icon, Menu } from 'semantic-ui-react';
 import Head from 'next/head';
 import DataFeed from '../components/datafeed-page';
 import BMR from '../components/bmr-page';
+import * as ethers from 'ethers';
 import { Link } from '../routes';
-class MoonLink extends Component {
-   // Nextjs uses this function to render this first server-side
-   static async getInitialProps() {
-      return {};
-   }
+import detectEthereumProvider from '@metamask/detect-provider';
 
-   // Initial State
-   state = {
-      account: 'Not Connected',
-   };
+const MoonlinkDashboard = () => {
+  // Initial State
+  const [account, setAccount] = useState('Not Connected');
+  const [connected, setConnected] = useState(false);
+  const [networkName, setNetworkName] = useState('Not Connected');
 
-   async componentDidMount() {
-      // If already connected display account
-      if (
-         typeof ethereum !== 'undefined' &&
-         ethereum.selectedAddress !== null
-      ) {
-         this.setState({ account: ethereum.selectedAddress });
+  useEffect(async () => {
+    await checkMetamask();
+
+    // Check for changes in Metamask (account and chain)
+    if (window.ethereum) {
+      window.ethereum.on('chainChanged', () => {
+        window.location.reload();
+      });
+      window.ethereum.on('accountsChanged', () => {
+        window.location.reload();
+      });
+    }
+  }, []);
+
+  const checkMetamask = async () => {
+    const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+
+    if (provider) {
+      const chainId = await provider.request({
+        method: 'eth_chainId',
+      });
+
+      let networkName;
+      switch (chainId) {
+        case '0x507':
+          networkName = 'Moonbase Alpha';
+          break;
+        default:
+          networkName = '';
+          setAccount('Only Moonbase Alpha Supported');
+          break;
       }
-   }
+      if (networkName !== '') {
+        setNetworkName(networkName);
+        const accounts = await ethereum.request({
+          method: 'eth_requestAccounts',
+        });
 
-   onConnect = async () => {
-      if (typeof ethereum === 'undefined') {
-         // MetaMask not detected
-         this.setState({ account: 'MetaMask not Detected' });
-      } else {
-         // MetaMask detected - check network
-         if (ethereum.chainId !== '0x507') {
-            this.setState({ account: 'Only Moonbase Alpha Supported' });
-         } else {
-            const accounts = await ethereum.request({
-               method: 'eth_requestAccounts',
-            });
-
-            // Set account to state
-            if (accounts) {
-               this.setState({ account: accounts[0] });
-            }
-         }
+        // Update State
+        if (accounts) {
+          setAccount(ethers.utils.getAddress(accounts[0]));
+          setConnected(true);
+        }
       }
-   };
+    } else {
+      // MetaMask not detected
+      setAccount('MetaMask not Detected');
+    }
+  };
 
-   render() {
-      return (
-         <Container>
-            <Head>
-               <title>Moonlink Dashboard</title>
-               <link
-                  rel='icon'
-                  type='image/png'
-                  sizes='32x32'
-                  href='/favicon.png'
-               />
-               <link
-                  rel='stylesheet'
-                  href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
-               />
-            </Head>
-            <Menu style={{ marginTop: '10px' }}>
-               <Link route='/'>
-                  <a className='item'>MoonLink Oracle Dashboard</a>
-               </Link>
-               <Menu.Menu position='right'>
-                  <a className='item'> {this.state.account} </a>
-                  <Button
-                     floated='right'
-                     content='Connect MetaMask'
-                     icon='plus square'
-                     primary
-                     onClick={this.onConnect}
-                  />
-               </Menu.Menu>
-            </Menu>
-            <br />
-            <DataFeed />
-            <br />
-            <hr />
-            <br />
-            <BMR account={this.state.account} />
-            <br />
-            <p>
-               Don't judge the code :) as it is for demostration purposes only.
-               You can check the source code &nbsp;
-               <a href='https://github.com/albertov19/moonlink-dashboard'>
-                  here
-               </a>
-            </p>
-            <br />
-         </Container>
-      );
-   }
-}
+  const onConnect = async () => {
+    await checkMetamask();
+  };
 
-export default MoonLink;
+  return (
+    <Container>
+      <Head>
+        <title>Moonrevoker</title>
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
+        <link
+          rel='stylesheet'
+          href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+        />
+      </Head>
+      <div style={{ paddingTop: '10px' }}></div>
+      <Menu>
+        <Link route='/'>
+          <a className='item'>Moonbase Alpha Chainlink Dashboard</a>
+        </Link>
+        <Menu.Menu position='right'>
+          <a className='item'> {account} </a>
+          {{ connected }.connected ? (
+            <Button floated='right' icon labelPosition='left' color='green'>
+              <Icon name='check'></Icon>
+              {networkName}
+            </Button>
+          ) : (
+            <Button floated='right' icon labelPosition='left' onClick={onConnect} primary>
+              <Icon name='plus square'></Icon>
+              Connect MetaMask
+            </Button>
+          )}
+        </Menu.Menu>
+      </Menu>
+      <br />
+      <DataFeed />
+      <br />
+      <hr />
+      <br />
+      <BMR account={account} />
+      <br />
+      <p>
+        Don't judge the code :) as it is for demostration purposes only. You can check the source
+        code &nbsp;
+        <a href='https://github.com/albertov19/moonlink-dashboard'>here</a>
+      </p>
+      <br />
+    </Container>
+  );
+};
+
+export default MoonlinkDashboard;
